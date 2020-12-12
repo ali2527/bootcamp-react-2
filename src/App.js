bada675c97e067db8cabc6b57d9c9737d465e2e7
ali Hussain <ali.talha3357@gmail.com>
2020-12-12 17:09:47 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Button from '@material-ui/core/Button';
+import { toggleDarkMode } from "halfmoon"
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <button className="btn m-10" onClick={toggleDarkMode}> &#127769;</button>
 
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" >
       Hello World
     </Button>
       
