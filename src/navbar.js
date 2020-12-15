5c1a271838192bd964d471026e31a9f3b116782f
ali Hussain <ali.talha3357@gmail.com>
2020-12-16 01:42:42 +0500

    resolving minor issues in container build

--- src/navbar.js
+++ src/navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
   return (
     <div>
       <nav className="navbar bg-primary bg-dark-dm">
-        <a href="#" className="navbar-brand text-light"><img src={icon} />Covid-19 Tracker</a>
+        <a href="https://github.com/ali2527/bootcamp-react-2" className="navbar-brand text-light"><img src={icon} alt="logo"/>Covid-19 Tracker</a>
 
         <form className="form-inline ml-auto" >
 
