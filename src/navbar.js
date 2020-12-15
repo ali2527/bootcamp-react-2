66c49bc0f05023ee0556c2c25fbf18bc4ac8f54c
ali Hussain <ali.talha3357@gmail.com>
2020-12-16 01:38:05 +0500

    removed search bar

--- src/navbar.js
+++ src/navbar.js
@@ -14,21 +14,7 @@ function Navbar() {
         <a href="#" className="navbar-brand text-light"><img src={icon} />Covid-19 Tracker</a>
 
         <form className="form-inline ml-auto" >
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search docs"
-              required="required"
-            />
-            <div className="input-group-append">
-              <button className="btn" type="submit">
-              &#x1F50D;
-                <span className="sr-only">Search docs</span>
-              </button>
-            </div>
-            
-          </div>
+          
           <Button onClick={toggleDarkmode}> &#127769;</Button>
         </form>
       </nav>
